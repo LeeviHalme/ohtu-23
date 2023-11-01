0ba747e7fc79e06c6c2a2a71278a306750e0eb5d
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-11-01 15:59:04 +0200

    add unit tests for the new filter

--- viikko-1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko-1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -1,15 +1,16 @@
 import unittest
 from statistics_service import StatisticsService
 from player import Player
+from sortby import SortBy
 
 class PlayerReaderStub:
     def get_players(self):
         return [
-            Player("Semenko", "EDM", 4, 12),
-            Player("Lemieux", "PIT", 45, 54),
-            Player("Kurri",   "EDM", 37, 53),
-            Player("Yzerman", "DET", 42, 56),
-            Player("Gretzky", "EDM", 35, 89)
+            Player("Semenko", "EDM", 4, 12), # 16
+            Player("Lemieux", "PIT", 45, 54), # 99
+            Player("Kurri",   "EDM", 37, 53), # 90
+            Player("Yzerman", "DET", 42, 56), # 98
+            Player("Gretzky", "EDM", 35, 89) # 124
         ]
 
 class TestStatisticsService(unittest.TestCase):
@@ -41,3 +42,28 @@ class TestStatisticsService(unittest.TestCase):
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0].name, "Gretzky")
+
+    def test_top_multiple(self):
+        result = self.service.top(2)
+
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].name, "Gretzky")
+        self.assertEqual(result[1].name, "Lemieux")
+    
+    def test_top_points(self):
+        result = self.service.top(1, SortBy.POINTS)
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, "Gretzky")
+    
+    def test_top_assists(self):
+        result = self.service.top(1, SortBy.ASSISTS)
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, "Gretzky")
+    
+    def test_top_goals(self):
+        result = self.service.top(1, SortBy.GOALS)
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, "Lemieux")
