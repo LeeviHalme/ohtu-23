d82eeecc6d32264ad7d6af5ea41b5511c0b4f842
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-11-20 23:47:27 +0200

    add exercises 1-3/week-3

---
+++ viikko-3/login-robot/src/services/user_service.py
@@ -0,0 +1,52 @@
+import re
+from entities.user import User
+
+class UserInputError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self._user_repository = user_repository
+
+    def check_credentials(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        user = self._user_repository.find_by_username(username)
+
+        if not user or user.password != password:
+            raise AuthenticationError("Invalid username or password")
+
+        return user
+
+    def create_user(self, username, password):
+        self.validate(username, password)
+
+        user = self._user_repository.create(
+            User(username, password)
+        )
+
+        return user
+
+    def validate(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if len(username) < 3:
+            raise UserInputError("Username too short")
+        
+        if re.match("^[a-z]+$", username) is None:
+            raise UserInputError("Username must contain only letters a-z")
+        
+        # Salasanan on oltava pituudeltaan vähintään 8 merkkiä ja se ei saa koostua pelkästään kirjaimista
+        if len(password) < 8:
+            raise UserInputError("Password too short")
+        
+        if re.match("^[a-zA-Z]+$", password) is not None:
+            raise UserInputError("Password must contain at least one non-letter character")
