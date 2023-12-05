8dc3f2f303f373e591908b8f9df07f6c26c477c7
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2023-12-05 15:28:32 +0200

    return exc. 4 & 5 / week 6

--- viikko-6/query-language/src/index.py
+++ viikko-6/query-language/src/index.py
@@ -1,21 +1,29 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All, Or
+from query_builder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
+    query = QueryBuilder()
 
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("FLA"),
-            PlaysIn("BOS")
-        )
+    m1 = (
+      query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
     )
 
+    m2 = (
+      query
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
